1062b7c5b08ee2431a5b093584dfe7f503b905e1
zhangz3462 <zhangz3462@my.sduhsd.net>
2026-03-04 19:51:11 +0000

    Added Contact/About

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// add javascript here
+document.getElementById("jokeToggle")?.addEventListener("click", () => {
+    document.getElementById("jokeText").toggleAttribute("hidden");
+})
+
+document.getElementById("funFact")?.addEventListener("click", () => {
+    document.getElementById("factText").toggleAttribute("hidden");
+})
